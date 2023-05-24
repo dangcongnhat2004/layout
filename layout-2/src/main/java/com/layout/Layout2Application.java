8e020f5b0d9a6f8aca503beff1b23e0ac50ca365
dangcongnhat2004 <125759308+dangcongnhat2004@users.noreply.github.com>
2023-05-24 08:47:53 +0700

    Add files via upload

---
+++ layout-2/src/main/java/com/layout/Layout2Application.java
@@ -0,0 +1,13 @@
+package com.layout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Layout2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Layout2Application.class, args);
+	}
+
+}
---
+++ layout-2/src/test/java/com/layout/Layout2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.layout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Layout2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
